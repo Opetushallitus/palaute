3de499b0d882615786a02efba3e2bbb67c97e430
Jussi Jartamo <jussi.jartamo@gofore.com>
2019-10-09 19:11:24 +0300

    absolute url to api

--- src/main/js/Results.jsx
+++ src/main/js/Results.jsx
@@ -14,7 +14,7 @@ const Results = (props) => {
         const runEffect = async () => {
             try {
                 const data = await fetch(
-                    "api/palaute?q=" + query,
+                    "/palaute/api/palaute?q=" + query,
                     {signal: controller.signal}
                 ).then(r => r.json());
                 setData(data);
