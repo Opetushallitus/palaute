00f766366f10ef9e7179d2183a738911db1f3e22
Jussi Jartamo <jussi.jartamo@gofore.com>
2019-10-31 09:13:24 +0200

    debug print removed

--- src/main/clojure/palaute/authentication/session_store.clj
+++ src/main/clojure/palaute/authentication/session_store.clj
@@ -7,22 +7,18 @@
 (defqueries "sql/session-queries.sql")
 
 (defn read-data [key]
-  (prn "reading with key " key)
   (when-let [data (:data (first (exec yesql-get-session-query {:key key})))]
     (assoc data :key key)))
 
 (defn add-data [key data]
-  (prn "adding key " key " with data " data)
   (exec yesql-add-session-query! {:key key :data (dissoc data :key)})
   key)
 
 (defn save-data [key data]
-  (prn "saving key " key " with data " data)
   (exec yesql-update-session-query! {:key key :data (dissoc data :key)})
   key)
 
 (defn delete-data [key]
-  (prn "deleting key " key )
   (exec yesql-delete-session-query! {:key key})
   key)
 
@@ -31,15 +27,12 @@
 (deftype DatabaseStore []
     SessionStore
   (read-session [a key]
-    (prn "READ" key )
     (read-data key))
   (write-session [_ key data]
-    (prn "WIT")
     (if key
       (save-data key data)
       (add-data (generate-new-random-key) data)))
   (delete-session [_ key]
-    (prn "DELETE")
     (delete-data key)
     nil))
 
