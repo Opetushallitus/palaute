20b82a0ad648e08af19e458ec312ac41b8417dd0
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-01-08 12:22:50 +0200

    excel cleanup

--- src/main/js/Excel.js
+++ src/main/js/Excel.js
@@ -5,21 +5,21 @@ import { saveAs } from 'file-saver';
 const Excel = (ws_data) => {
     const wb = XLSX.utils.book_new();
     wb.Props = {
-        Title: "SheetJS Tutorial",
-        Subject: "Test",
-        Author: "Red Stapler",
-        CreatedDate: new Date(2017,12,19)
+        Title: "Palaute",
+        Subject: "Palaute",
+        Author: "Palautepalvelu",
+        CreatedDate: new Date()
     };
     wb.SheetNames.push("Test Sheet");
     wb.Sheets["Test Sheet"] = XLSX.utils.aoa_to_sheet(ws_data);
     const wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'});
     function s2ab(s) {
-        var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
-        var view = new Uint8Array(buf);  //create uint8array as viewer
-        for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
         return buf;
     }
-    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'test.xlsx');
+    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'palaute.xlsx');
 };
 
 export default Excel;
