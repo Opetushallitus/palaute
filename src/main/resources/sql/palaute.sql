9116f752a75db2e31aa337f2b09cea267a0bb40f
August Kilponen <august.kilponen@gmail.com>
2021-09-02 10:30:20 +0300

    Listataan palautteet aikajärjestyksessä (uusimmat ensin)

--- src/main/resources/sql/palaute.sql
+++ src/main/resources/sql/palaute.sql
@@ -19,7 +19,7 @@ INSERT INTO feedback (
 );
 
 -- name: yesql-get-feedback
-SELECT (extract(epoch from created_time)::bigint) * 1000 as created_time, stars, user_agent, feedback FROM feedback WHERE key LIKE ?;
+SELECT (extract(epoch from created_time)::bigint) * 1000 as created_time, stars, user_agent, feedback FROM feedback WHERE key LIKE ? ORDER BY created_time DESC;
 
 -- name: yesql-get-average
 SELECT avg(stars) as keskiarvo, count(stars) as lukumaara FROM feedback WHERE key = :key;
